03aa996d42d04fbf5ee29bad67b3e5e89c9befa1
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-04-22 13:29:45 +0800

    feat: added hero, gender, essentials

---
+++ scripts/home.js
@@ -0,0 +1,28 @@
+let xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let xmlDoc = this.responseXML;
+
+    let xslhttp = new XMLHttpRequest();
+    xslhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let xslDoc = this.responseXML;
+
+        //Apply XSLT transformation
+        let xsltProcessor = new XSLTProcessor();
+        xsltProcessor.importStylesheet(xslDoc);
+
+        let resultDoc = xsltProcessor.transformToFragment(xmlDoc, document);
+
+        document.querySelector(".essentials").appendChild(resultDoc);
+      }
+    };
+
+    xslhttp.open("GET", "products.xsl", true);
+    xslhttp.send();
+  }
+};
+
+xmlhttp.open("GET", "products.xml", true);
+xmlhttp.send();
