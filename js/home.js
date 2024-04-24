13647a45b4c48dbefb95423cfbe1f43d1e29a685
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-04-24 17:30:49 +0800

    refactor: switched to jquery

--- js/home.js
+++ js/home.js
@@ -1,28 +1,16 @@
-let xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    let xmlDoc = this.responseXML;
-
-    let xslhttp = new XMLHttpRequest();
-    xslhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        let xslDoc = this.responseXML;
-
-        //Apply XSLT transformation
+$.ajax({
+  url: "products.xml",
+  dataType: "xml",
+  success: function (xmlDoc) {
+    $.ajax({
+      url: "products.xsl",
+      dataType: "xml",
+      success: function (xslDoc) {
         let xsltProcessor = new XSLTProcessor();
         xsltProcessor.importStylesheet(xslDoc);
-
         let resultDoc = xsltProcessor.transformToFragment(xmlDoc, document);
-
-        document.querySelector(".essentials-products").appendChild(resultDoc);
-      }
-    };
-
-    xslhttp.open("GET", "products.xsl", true);
-    xslhttp.send();
-  }
-};
-
-xmlhttp.open("GET", "products.xml", true);
-xmlhttp.send();
+        $(".essentials-products").append(resultDoc);
+      },
+    });
+  },
+});
